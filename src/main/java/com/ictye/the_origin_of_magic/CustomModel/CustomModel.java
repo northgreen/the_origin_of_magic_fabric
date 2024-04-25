2eb8db6b2031edbea417677f738727b07624b939
ictye <northgreen2006@qq.com>
2024-04-25 23:06:22 +0800

    修复了代码中的包导入问题，并添加了新的抽象类StdDriestEffectMagic。

---
+++ src/main/java/com/ictye/the_origin_of_magic/CustomModel/CustomModel.java
@@ -0,0 +1,32 @@
+// Made with Blockbench 4.9.4
+// Exported for Minecraft version 1.17+ for Yarn
+// Paste this class into your mod and generate all required imports
+
+package com.ictye.the_origin_of_magic.CustomModel;
+
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+
+public class CustomModel extends EntityModel<Entity> {
+	private final ModelPart bb_main;
+	public CustomModel(ModelPart root) {
+		this.bb_main = root.getChild("bb_main");
+	}
+	TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(-12, -12).cuboid(-7.0F, -14.0F, -7.0F, 14.0F, 14.0F, 14.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+		return TexturedModelData.of(modelData, 16, 16);
+	}
+	@Override
+	public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	}
+	@Override
+	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+		bb_main.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+	}
+
+}
